be76de33057993680021a56160e2c2fe4bcb4e02
tkmoney11 <tylerkim@outlook.com>
2020-07-02 13:31:58 -0700

    Almost done with Homepage

--- src/Widgets/Home/Home.js
+++ src/Widgets/Home/Home.js
@@ -1,13 +1,19 @@
 import React, {Component} from 'react'
 import './Home.css'
-import LoadingIcon from './../../Components/LoadingIcon.js';
+import TKViolin from './../../assets/thresholds/tkviolin';
 
 class Home extends Component {
 
     render() {
         return (
             <div className="home_page">
-                <LoadingIcon />
+                <TKViolin />
+                <div className="home_msg">
+                    <span id="first_line">I'm <span className="highlight">seeking employment,</span></span><span>but you can call me <span className="highlight">Tyler</span></span>
+                </div>
+                <a href="src\assets\resume\TylerKim2020.docx" download="Tylers Resume.docx">
+                    <div className="resume">RESUME</div>
+                </a>
             </div>
         )
     }
