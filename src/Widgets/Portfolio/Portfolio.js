3c1f00865756218a694d681780fe401a6b244f4a
tkmoney11 <tylerkim@outlook.com>
2020-07-07 15:05:13 -0700

    updated portfolio page

--- src/Widgets/Portfolio/Portfolio.js
+++ src/Widgets/Portfolio/Portfolio.js
@@ -86,22 +86,17 @@ class Portfolio extends Component {
     render() {
         return (
             <div className="portfolio_page">
+                <div className="tmp_msg"></div>
                 <div className="filter_header">
                     <FilterButton filteredIn={this.state.filteredIn} filteredOut={this.state.filteredOut} text="Web Development" onClick={(urmom) => this.handleSelect(urmom)} selected={this.state.selected} />
                     <FilterButton filteredIn={this.state.filteredIn} filteredOut={this.state.filteredOut} text="React" onClick={(urmom) => this.handleSelect(urmom)} selected={this.state.selected} />
                     <FilterButton filteredIn={this.state.filteredIn} filteredOut={this.state.filteredOut} text="JavaScript" onClick={(urmom) => this.handleSelect(urmom)} selected={this.state.selected} />
-                    <FilterButton filteredIn={this.state.filteredIn} filteredOut={this.state.filteredOut} text="Other" onClick={(urmom) => this.handleSelect(urmom)} selected={this.state.selected} />
+                    <FilterButton filteredIn={this.state.filteredIn} filteredOut={this.state.filteredOut} text="Temp Filter Demo" onClick={(urmom) => this.handleSelect(urmom)} selected={this.state.selected} />
                 </div>
                 <div className="project_container">
                     <ProjectPreview display={this.state.selected.length < 1 ? null :
                     !this.state.selected.every(el => (this.state.filterCombos[0]).includes(el))? "hide_project" : null} 
                     title="Portfolio" src="Portfolio.PNG" url="https://tylersportfolio.netlify.app/" stack="React JavaScript"/>
-                    <ProjectPreview display={this.state.selected.length < 1 ? null :
-                    !this.state.selected.every(el => (this.state.filterCombos[1]).includes(el))? "hide_project" : null} 
-                    title="Other" src="Portfolio.PNG" url="https://tylersportfolio.netlify.app/" stack="JavaScript"/>
-                    <ProjectPreview display={this.state.selected.length < 1 ? null :
-                    !this.state.selected.every(el => (this.state.filterCombos[2]).includes(el))? "hide_project" : null} 
-                    title="Third" src="Portfolio.PNG" url="https://tylersportfolio.netlify.app/" stack="React JavaScript"/>
                 </div>
             </div>
         )
