1c28289aa011b9a3d7c8f4fc03e9daaa88d92859
tkmoney11 <tylerkim@outlook.com>
2020-07-17 20:22:20 -0700

    Undid load icon changes

--- src/Widgets/About/About.js
+++ src/Widgets/About/About.js
@@ -12,19 +12,14 @@ class About extends Component {
                     <div className="name">TYLER KIM</div>
                     <div className='role'>Software Engineer</div>   
                 </div>
-                <InvDelayed waitBeforeHide={500}> 
-                    <LoadingIcon />
-                </InvDelayed>
-                <Delayed waitBeforeShow={500}> 
-                    <div className="content">
-                        <div className='about_box'>
-                            <div>In addition to software development, I can play violin for you or a loved one's wedding! </div>
-                            <br />
-                            <div className="shoutout">Shoutout to <a className="jeannine" href="https://www.jeanninepoundphotography.com/" target="_blank"><span id="backdrop">Jeannine Pound Photography.</span></a> She does a marvelous job.</div>
-                        </div>
-                        <img className="about_img" src="TylerViolin.jpg" alt=""/>
+                <div className="content">
+                    <div className='about_box'>
+                        <div>In addition to software development, I can play violin for you or a loved one's wedding! </div>
+                        <br />
+                        <div className="shoutout">Shoutout to <a className="jeannine" href="https://www.jeanninepoundphotography.com/" target="_blank"><span id="backdrop">Jeannine Pound Photography.</span></a> She does a marvelous job.</div>
                     </div>
-                </Delayed>
+                    <img className="about_img" src="TylerViolin.jpg" alt=""/>
+                </div>
             </div>
         )
     }
