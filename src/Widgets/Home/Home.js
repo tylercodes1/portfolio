dca7c58f2c7319a54342b72a178d476f562c2a21
tkmoney11 <tylerkim@outlook.com>
2021-03-12 20:46:21 -0800

    Update title to reflect occupation

--- src/Widgets/Home/Home.js
+++ src/Widgets/Home/Home.js
@@ -4,44 +4,47 @@ import TKViolin from "./../../assets/thresholds/tkviolin";
 import BlockButton from "../Misc/Buttons/BlockButton";
 
 class Home extends Component {
-  constructor() {
-    super();
-    this.state = {
-      pressed: false,
-    };
-  }
+	constructor() {
+		super();
+		this.state = {
+			pressed: false,
+		};
+	}
 
-  handleClick() {
-    this.setState(() => {
-      return {
-        pressed: true,
-      };
-    });
-  }
+	handleClick() {
+		this.setState(() => {
+			return {
+				pressed: true,
+			};
+		});
+	}
 
-  render() {
-    return (
-      <div className="home_page">
-        <TKViolin />
-        <div className="home_msg">
-          <span id="first_line">
-            I'm <span className="highlight">seeking employment,</span>
-          </span>
-          <span>
-            but you can call me <span className="highlight">Tyler.</span>
-          </span>
-        </div>
-        <a
-          className={"btn" + (this.state.pressed ? " btn_pressed" : "")}
-          onClick={() => this.handleClick()}
-          href="TylerKim2021.pdf"
-          download="kim_tyler.pdf"
-        >
-          <BlockButton text="RESUME" pressed={this.state.pressed}></BlockButton>
-        </a>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="home_page">
+				<TKViolin />
+				<div className="home_msg">
+					<span id="first_line">
+						I'm <span className="highlight">a software engineer,</span>
+					</span>
+					<span>
+						but you can call me <span className="highlight">Tyler.</span>
+					</span>
+				</div>
+				<a
+					className={"btn" + (this.state.pressed ? " btn_pressed" : "")}
+					onClick={() => this.handleClick()}
+					href="TylerKim2021.pdf"
+					download="kim_tyler.pdf"
+				>
+					<BlockButton
+						text="RESUME"
+						pressed={this.state.pressed}
+					></BlockButton>
+				</a>
+			</div>
+		);
+	}
 }
 
 export default Home;
