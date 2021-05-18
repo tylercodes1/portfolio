4ec2f62b5a828ea5f9372515b9d59c55df3667bb
tkmoney11 <tylerkim@outlook.com>
2021-05-18 09:03:15 -0700

    updated contacts

--- src/Widgets/Contact/Contact.js
+++ src/Widgets/Contact/Contact.js
@@ -1,36 +1,45 @@
-import React, {Component} from 'react'
-import './Contact.css'
-import Linkedin from './../../assets/linkedin/linkedin';
-import Mail from '../../assets/generic/mail/Mail';
-import Phone from '../../assets/generic/phone/Phone';
+import React, { Component } from "react";
+import "./Contact.css";
+import Linkedin from "./../../assets/linkedin/linkedin";
+import Mail from "../../assets/generic/mail/Mail";
+import Phone from "../../assets/generic/phone/Phone";
 
 class Contact extends Component {
-    render() {
-        return (
-            <div className="contact_page">
-                <div className="contact_box">
-                    <div className="contact_head"><span id="hi">Hi!</span> You can reach me at:</div>
-                    <div className="contact_items">
-                        <a target="_blank" href="https://www.linkedin.com/in/tylerkim11/">
-                            <div className="contact_item">
-                                <Linkedin class="c_linkedin" id1="c_linkedin1" id2="c_linkedin2"/>
-                            </div>
-                        </a>
-                        <a target="_blank" href="mailto: tylerkim@outlook.com">
-                            <div className="contact_item">
-                                <Mail class="c_mail" id="c_mail"/>
-                            </div>
-                        </a>
-                        <a href='tel:4259228611'>
-                            <div className="contact_item">
-                                <Phone class="c_phone" id="c_phone"/>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div className="contact_page">
+				<div className="contact_box">
+					<div className="contact_head">
+						<span id="hi">Hi!</span> You can reach me at:
+					</div>
+					<div className="contact_items">
+						<a
+							target="_blank"
+							href="https://www.linkedin.com/in/tylerkim11/"
+						>
+							<div className="contact_item">
+								<Linkedin
+									class="c_linkedin"
+									id1="c_linkedin1"
+									id2="c_linkedin2"
+								/>
+							</div>
+						</a>
+						<a target="_blank" href="mailto: tylerkim@outlook.com">
+							<div className="contact_item">
+								<Mail class="c_mail" id="c_mail" />
+							</div>
+						</a>
+						<a href="tel:4259791760">
+							<div className="contact_item">
+								<Phone class="c_phone" id="c_phone" />
+							</div>
+						</a>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
-export default Contact
+export default Contact;
--- src/Widgets/General/Nav2/Nav2.js
+++ src/Widgets/General/Nav2/Nav2.js
@@ -1,33 +1,45 @@
-import React, {Component} from 'react'
-import './Nav2.css'
-import {BrowserRouter as Router, Route, Link, useLocation } from 'react-router-dom'
-import Nav2Item from './Nav2Item';
-import SocialItem from './SocialItem';
-import Github from './../../../assets/github/github';
-import Linkedin from './../../../assets/linkedin/linkedin';
-import PersonalLogo from '../../../assets/personal_logo/personallogo';
+import React, { Component } from "react";
+import "./Nav2.css";
+import {
+	BrowserRouter as Router,
+	Route,
+	Link,
+	useLocation,
+} from "react-router-dom";
+import Nav2Item from "./Nav2Item";
+import SocialItem from "./SocialItem";
+import Github from "./../../../assets/github/github";
+import Linkedin from "./../../../assets/linkedin/linkedin";
+import PersonalLogo from "../../../assets/personal_logo/personallogo";
 
 function Nav2() {
-    let location = useLocation()
-    return (
-        <div className="navbar">
-            <Link className="logo" to="/home">
-                <PersonalLogo />
-            </Link>
-            <div className="nav_items">
-                <Nav2Item link="home" active={location.pathname} />   
-                <Nav2Item link="portfolio" active={location.pathname} />  
-                <Nav2Item link="skills" active={location.pathname} />
-                <Nav2Item link="about" active={location.pathname} />
-                <Nav2Item link="contact" active={location.pathname} />
-            </div>
-            <div className="social_items">
-                <SocialItem icon={<Github />} url="https://github.com/tkmoney11"></SocialItem>
-                <SocialItem icon={<Linkedin class="linkedin" id1="linkedin1" id2="linkedin2"/>}  url="https://www.linkedin.com/in/tylerkim11/"></SocialItem>
-            </div>
-        </div>
-        
-    )
+	let location = useLocation();
+	return (
+		<div className="navbar">
+			<Link className="logo" to="/home">
+				<PersonalLogo />
+			</Link>
+			<div className="nav_items">
+				<Nav2Item link="home" active={location.pathname} />
+				<Nav2Item link="portfolio" active={location.pathname} />
+				<Nav2Item link="skills" active={location.pathname} />
+				<Nav2Item link="about" active={location.pathname} />
+				<Nav2Item link="contact" active={location.pathname} />
+			</div>
+			<div className="social_items">
+				<SocialItem
+					icon={<Github />}
+					url="https://github.com/tylercodes1"
+				></SocialItem>
+				<SocialItem
+					icon={
+						<Linkedin class="linkedin" id1="linkedin1" id2="linkedin2" />
+					}
+					url="https://www.linkedin.com/in/tylerkim11/"
+				></SocialItem>
+			</div>
+		</div>
+	);
 }
 
-export default Nav2
+export default Nav2;
