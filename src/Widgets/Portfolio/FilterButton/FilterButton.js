7e71d6eb912d7c5fb289b59b905ff86734d3d464
tkmoney11 <tylerkim@outlook.com>
2021-06-13 22:59:20 -0700

    Whole word filtering

--- src/Widgets/Portfolio/FilterButton/FilterButton.js
+++ src/Widgets/Portfolio/FilterButton/FilterButton.js
@@ -1,25 +1,34 @@
-import React from 'react'
-import './FilterButton.css'
-
+import React from "react";
+import "./FilterButton.css";
 
 function FilterButton(props) {
-    return(
-        <div className={"filter_button" + (props.selected.includes(props.text) ? " filter_select" : 
-        (isFilteredIn(props.filteredIn, props.filteredOut, props.text) ? ' filtered_out': ' i am a penis'))} 
-        onClick={() => props.onClick(props.text)}>{props.text}</div>
-    )
+	return (
+		<div
+			className={
+				"filter_button" +
+				(props.selected.includes(props.text)
+					? " filter_select"
+					: isFilteredIn(props.filteredIn, props.filteredOut, props.text)
+					? " filtered_out"
+					: " i am a penis")
+			}
+			onClick={() => props.onClick(props.text)}
+		>
+			{props.text}
+		</div>
+	);
 }
 
-// filtered out logic: checks if button is still available to be pressed: 
+// filtered out logic: checks if button is still available to be pressed:
 // are available projects 'filtered in'
 function isFilteredIn(fi, fo, txt) {
-    if (fo.length < 1) {
-        return false
-    } else {
-        return !fi.some(element => {
-            return element.includes(txt)
-        })
-    }
+	if (fo.length < 1) {
+		return false;
+	} else {
+		return !fi.some((element) => {
+			return element.split(" ").includes(txt);
+		});
+	}
 }
 
-export default FilterButton
+export default FilterButton;
